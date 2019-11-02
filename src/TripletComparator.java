6aeee4a7e5eb8aa8cd344cea00bbbc20f1b5c630
Ankit <ankbaid@gmail.com>
2019-11-02 13:49:09 +0530

    compare the triplet

--- src/TripletComparator.java
+++ src/TripletComparator.java
@@ -1,14 +1,25 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class TripletComparator {
     static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
-        return null;
+        int aWins = 0;
+        int bWins = 0;
+        for (int i = 0; i < a.size(); i++) {
+            int aValue = a.get(i);
+            int bValue = b.get(i);
+            int compare = Integer.compare(aValue, bValue);
+            if(compare > 0) aWins++;
+            if(compare < 0) bWins++;
+        }
+        return Arrays.asList(aWins, bWins);
     }
 
     public static void main(String[] args) {
-        List<Integer> a = Arrays.asList(2,2,2);
-        List<Integer> b = Arrays.asList(1,1,1);
+        List<Integer> a = Arrays.asList(17, 28, 30);
+        List<Integer> b = Arrays.asList(99, 16, 8);
         System.out.println(compareTriplets(a, b));
     }
 }
