f9d844fc54105242148cdb73ceabc331b035ff96
Ankit <ankbaid@gmail.com>
2019-11-02 13:14:02 +0530

    setup

---
+++ src/TripletComparator.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class TripletComparator {
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> a = Arrays.asList(2,2,2);
+        List<Integer> b = Arrays.asList(1,1,1);
+        System.out.println(compareTriplets(a, b));
+    }
+}
